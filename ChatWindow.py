c5bcbce3c5f3a0c25bd345d6dbe6e9e61dd167e5
Netmme <38214932+netmme@users.noreply.github.com>
2018-10-20 13:57:24 +0200

    test ui

--- ChatWindow.py
+++ ChatWindow.py
@@ -19,11 +19,16 @@ class ChatWindow(object):
 		self.menubar = Menu(self.win_chat)
 		self.menu1 = Menu(self.menubar, tearoff=0)
 		self.menu1.add_command(label="Quitter", command=self.win_chat.quit)
-		#self.menu1.add_separator()
+		self.menu1.add_separator()
 		self.menu1.add_command(label="Aide", command=self.win_chat.quit)
-		self.win_chat.config(menu=self.menubar)
+		#self.win_chat.config(menu=self.menubar)
 
 		self.chat_pan = PanedWindow(self.win_chat, orient=VERTICAL)
+
+		#self.value = StringVar() 
+		#self.value.set("texte par défaut")
+		#self.entree = Entry(self.chat_pan, textvariable=self.value, heigth=5,width=30)
+		#self.entree.pack()
 
 		#txt_chat = Text(chat_pan, height=2, width=30)
 		self.txt_chat = Text(self.chat_pan)
@@ -38,12 +43,24 @@ class ChatWindow(object):
 		self.chat_pan.add(self.txt_send)
 		self.chat_pan.add(self.btn_send)
 		self.chat_pan.pack()
-		#self.win_chat.pack()
+
 		
 		self.win_chat.mainloop()
+		self.connect_msg()
+
+	def connect_msg(self):
+		print("exec")
+		msg = "Tototo\nlele"
+		content = self.txt_chat.get()
+		content += "toto"
+		self.txt_chat.set(content)
+		#self.txt_chat.config(state=NORMAL)
+		#self.txt_chat.set(msg)
+		#self.txt_chat.config(state=DISABLED)
+
 
 	def post_message(self):
 		print("toto")
 	
 if __name__ == "__main__":
-	login = LoginWindow()
+	print("main...")
